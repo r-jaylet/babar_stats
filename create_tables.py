47502f34d1f6f16dc029e84944e697d10a77dfa9
Rémi Jaylet <remi.jaylet@hotmail.com>
2022-10-02 22:21:20 +0200

    Data anonymization

--- create_tables.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect("babar_tables")
-cursor = connection.cursor()
-
-sql_file = open("babar3_modified.sql")
-sql_as_string = sql_file.read()
-cursor.executescript(sql_as_string)
